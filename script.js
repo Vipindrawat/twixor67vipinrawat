c68221fc688b12249104a1c6cf18c3442c80903e
Vipindrawat <102280089+Vipindrawat@users.noreply.github.com>
2026-04-15 14:44:16 +0530

    DAY-3/Javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+let button = document.getElementById("btn");
+button.addEventListener('click', () => {
+    alert("Watch now button is clicked");
+})
+
+let home = document.getElementById("home");
+home.addEventListener('click', () => {
+    alert("We are in Home");
+})
+
+let search = document.getElementById("search");
+search.addEventListener('click', () => {
+    alert("We are in search");
+})
+
+let TV = document.getElementById("TV");
+TV.addEventListener('click', () => {
+    alert("We are in TV");
+})
+
+let movies = document.getElementById("movies");
+movies.addEventListener('click', () => {
+    alert("We are in movies");
+})
+
+let sports = document.getElementById("sports");
+sports.addEventListener('click', () => {
+    alert("We are in sports");
+})
+
+let categories = document.getElementById("categories");
+categories.addEventListener('click', () => {
+    alert("We are in categories");
+})
+
+let myspace = document.getElementById("myspace")
+myspace.addEventListener('click', () => {
+    alert("We are in myspace");
+})
